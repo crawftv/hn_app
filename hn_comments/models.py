a7d00dd83913db53c8c265007a96063d2408736a
Crawford <crawford.collins@gmail.com>
2019-03-14 12:53:49 -0500

    added some utilities, fixed models, comments are on heroku.

--- hn_comments/models.py
+++ hn_comments/models.py
@@ -4,15 +4,10 @@ DB = SQLAlchemy()
 
 
 class Comments(DB.Model):
-    
+
     user_id = DB.Column(DB.String(15))
-    
-    deleted = DB.Column(DB.Boolean)
-    id = DB.Column(DB.BigInteger,primary_key=True)
-    kids = DB.Column(DB.String)
-    parent = DB.Column(DB.Integer)
+    comment_id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Text)
-    time = DB.Column(DB.String(10))
     neg = DB.Column(DB.Float)
     neu = DB.Column(DB.Float)
     pos = DB.Column(DB.Float)
