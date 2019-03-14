4faa2636a9c6381253c66d7d538405604025ff6f
Crawford <crawford.collins@gmail.com>
2019-03-14 13:07:19 -0500

    ready to push to heroku, fingers crossed

--- hn_comments/app.py
+++ hn_comments/app.py
@@ -1,25 +1,31 @@
 from flask import Flask, request, render_template, jsonify
+from decouple import config
 from flask_sqlalchemy import SQLAlchemy
 from .models import DB, Comments
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hn.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     DB.init_app(app)
+
     @app.route('/')
     def root():
         return "root"
 
-    @app.route('/user_lookup/<user_id>' ,methods=['GET'])
+    @app.route('/user_lookup/<user_id>', methods=['GET'])
     def user_lookup(user_id):
-        
+
         if request.method == "GET":
             results = []
-            query = DB.session.query(Comments).filter_by(user_id= user_id).limit(5).all()
+            query = DB.session.query(Comments).filter_by(
+                user_id=user_id).limit(5).all()
             for q in query:
-                result = {"user_id" : q.user_id, "text" : q.text, "neg":q.neg,"neu":q.neu, "pos":q.pos, "compound" : q.compound, "sentiment" : q.sentiment}
+                result = {"user_id": q.user_id, "text": q.text, "neg": q.neg, "neu": q.neu,
+                          "pos": q.pos, "compound": q.compound, "sentiment": q.sentiment}
                 results.append(result)
-            
 
         return jsonify(results)
     return app
