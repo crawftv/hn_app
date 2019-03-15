f9ab1ca1b620870f006e9b48ca85af3a29d1170b
Crawford <crawford.collins@gmail.com>
2019-03-14 19:51:07 -0500

    saltiest hacker added, everything else changed a bit

--- hn_comments/app.py
+++ hn_comments/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, render_template, jsonify
 from decouple import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from sqlalchemy import func
 from .models import DB, Comments
+import functools
 
 
 def create_app():
@@ -21,14 +23,37 @@ def create_app():
     def user_lookup(user_id):
 
         if request.method == "GET":
-            results = []
             query = DB.session.query(Comments).filter_by(
-                user_id=user_id).limit(5).all()
-            for q in query:
-                result = {"user_id": q.user_id, "text": q.text, "neg": q.neg, "neu": q.neu,
-                          "pos": q.pos, "compound": q.compound, "sentiment": q.sentiment}
-                results.append(result)
+                user_id=user_id).limit(500).all()
+            compound_sentiment = functools.reduce(
+                lambda x, y: x+y, [q.compound for q in query]) / num_results
+            # todo saltiest comments
+            # result = list(map(lambda x:
+            #                   {"user_id": x.user_id, "text": x.text,
+            #                    "compound_sentiment": x.compound},
+            #                   query))
 
-        return jsonify(results)
+        # return jsonify(result)
+        return jsonify(user_average_sentiment=compound_sentiment)
 
+    @app.route('/topic_sentiment/<topic>', methods=['GET'])
+    def topic_sentiment(topic):
+
+        if request.method == "GET":
+            results = []
+            query = DB.session.query(Comments
+                                     ).filter(Comments.text.like('%'+topic+'%')).limit(2500).all()
+            num_results = len(query)
+
+            compound_sentiment = functools.reduce(
+                lambda x, y: x+y, [q.compound for q in query]) / num_results
+        return jsonify(compound_sentiment=compound_sentiment)
+
+    @app.route('/saltiest_commenters', methods=["GET"])
+    def saltiest_commenters():
+        if request.method == "GET":
+            query = DB.session.query(Comments.user_id, func.count(
+                Comments.user_id), func.avg(Comments.compound)).group_by(Comments.user_id).having(func.count(Comments.user_id) > 49).order_by(func.avg(Comments.compound).asc()).limit(10).all()
+
+        return jsonify(query)
     return app
--- hn_comments/models.py
+++ hn_comments/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 DB = SQLAlchemy()
 
