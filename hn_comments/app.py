b46a88fca06d65bebe63715376b0ca7397cf829a
crawford <crawford.collins@gmail.com>
2019-04-19 19:46:49 -0500

    app is updated for new data

--- hn_comments/app.py
+++ hn_comments/app.py
@@ -41,26 +41,26 @@ def create_app():
                 Comments.text.like('%'+topic+'%')).limit(2500).all()
             num_results = len(query)
             if num_results > 0:
-                compound = [q.compound for q in query]
-                hist = np.histogram(compound, bins=10, range=(-1,1))
+                sentiment = [q.sentiment for q in query]
+                hist = np.histogram(sentiment, bins=10, range=(-1,1))
                 data = json.dumps([int(hist[0][i])/num_results*100  for i in range(len(hist[0]))] )
-                compound_sentiment = functools.reduce(
-                    lambda x, y: x+y, compound) / num_results
+                sentiment = functools.reduce(
+                    lambda x, y: x+y, sentiment) / num_results
                 return render_template("topic_sentiment.html",
-                compound_sentiment =compound_sentiment, topic= topic, data = data)
+                sentiment =sentiment, topic= topic, data = data)
             else:
                 return render_template("no_results.html")
     @app.route('/user_sentiment', methods=['GET'])
     def user_sentiment():
         user_id = request.values["user_id"]
         query = DB.session.query(Comments).filter_by(
-                user_id=user_id).limit(500).all()
+               by =user_id).limit(500).all()
         if len(query)> 0:
             def avg_sentiment(query):
                 num_results = len(query)
-                compound_sentiment = functools.reduce(
-                    lambda x, y: x+y, [q.compound for q in query]) / num_results
-                compound_sentiment = json.dumps(compound_sentiment)
+                sentiment = functools.reduce(
+                    lambda x, y: x+y, [q.sentiment for q in query]) / num_results
+                sentiment = json.dumps(sentiment)
                 return compound_sentiment
             def sentiment_dictionary(query):
                 sentiment = [q.sentiment for q in query]
@@ -105,16 +105,16 @@ def create_app():
 
         def avg_sentiment(user_id):
             query = DB.session.query(Comments).filter_by(
-                user_id=user_id).limit(500).all()
+                by=user_id).limit(500).all()
             num_results = len(query)
-            compound_sentiment = functools.reduce(
-                lambda x, y: x+y, [q.compound for q in query]) / num_results
+            sentiment = functools.reduce(
+                lambda x, y: x+y, [q.sentiment for q in query]) / num_results
 
-            return compound_sentiment
+            return sentiment
 
         def top_10_saltiest_comments(user_id): 
-            top_10 = DB.session.query(Comments.text, Comments.compound).filter_by(
-                user_id=user_id).order_by(Comments.compound.asc()).limit(10).all()
+            top_10 = DB.session.query(Comments.text, Comments.sentiment).filter_by(
+                by=user_id).order_by(Comments.compound.asc()).limit(10).all()
             return top_10
 
         return jsonify(user_average_sentiment=avg_sentiment(user_id), top_10=top_10_saltiest_comments(user_id))
@@ -126,15 +126,17 @@ def create_app():
             query = DB.session.query(Comments).filter(
                 Comments.text.like('%'+topic+'%')).limit(2500).all()
             num_results = len(query)
-            compound_sentiment = functools.reduce(
-                lambda x, y: x+y, [q.compound for q in query]) / num_results
-        return jsonify(compound_sentiment=compound_sentiment)
+            sentiment = functools.reduce(
+                lambda x, y: x+y, [q.sentiment for q in query]) / num_results
+        return jsonify(sentiment=sentiment)
 
     @app.route('/saltiest_commenters', methods=["GET"])
     def saltiest_commenters():
         if request.method == "GET":
-            query = DB.session.query(Comments.user_id, func.count(
-                Comments.user_id), func.avg(Comments.compound)).group_by(Comments.user_id).having(func.count(Comments.user_id) > 49).order_by(func.avg(Comments.compound).asc()).limit(10).all()
+            query = DB.session.query(Comments.sentiment, func.count(
+                Comments.by),
+                func.avg(Comments.sentiment)).group_by(Comments.by).having(func.count(Comments.by)
+                        > 49).order_by(func.avg(Comments.sentiment).asc()).limit(10).all()
 
         return jsonify(query)
 
